87f54ca0d751ece6fade460dc12dbb310c89b02e
renato <felicio95.jr@gmail.com>
2024-07-10 09:18:01 -0300

    feat(python): adds main script to improve module import at root project

---
+++ python/main.py
@@ -0,0 +1,8 @@
+import sys
+import os
+
+# Pyhton anti-design pattern: Add files directly to the PYTHONPATH
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+from src.search.binary_search import binary_search  # noqa
